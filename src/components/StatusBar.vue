b2892058eaf5b34e318cbe345accdcc7e0eaebb1
0xBA5E64 <26796481+0xBA5E64@users.noreply.github.com>
2023-01-18 19:51:22 +0100

    Remove Vue demo files & replace with basic view structure

---
+++ src/components/StatusBar.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>
+    <h1>This is the Statusbar</h1>
+  </div>
+</template>
+
+<style scoped>
+div {
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  background: #222;
+  border-top: 1px #444 solid;
+  width: 100%;
+  height: 40px;
+
+  padding: 0 16px;
+  box-sizing: border-box;
+
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-start;
+  align-items: center;
+
+  color: #DDD;
+  font-family: 'Roboto Mono', Courier, monospace;
+  text-transform: uppercase;
+}
+
+div * {
+  margin: 0;
+  font-size: 12px;
+}
+</style>
---
+++ src/views/SplashView.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1>Welcome to<br><span>TailChat.</span></h1>
+  </main>
+</template>
+
+<style scoped>
+
+main {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+h1 {
+  font-weight: normal;
+  font-size: 48px;
+}
+
+h1 span {
+  font-weight: bold;
+  font-size: 2em;
+  color: #0000;
+  background: linear-gradient(-15deg, #48F, #8F4);
+  -webkit-background-clip: text;
+  background-clip: text;
+}
+
+</style>
